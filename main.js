99951ff1a084a7cf614e24c4758b8fcabfdd3502
Tomasz Czechlewski <tomasz.czechlewski@gmail.com>
2018-10-15 20:48:40 +0200

    automatic slideshow ready

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    let slideShow = document.querySelector(".slideShow").children;
+    let counter = 0;
+    slideShow[counter].className = "showSlide";
+
+    setInterval(function () {
+        if (counter >= slideShow.length - 1) {
+            slideShow[counter].classList.toggle("showSlide");
+            counter = 0;
+            slideShow[counter].classList.toggle("showSlide");
+        } else {
+            slideShow[counter].classList.toggle("showSlide");
+            counter ++;
+            slideShow[counter].classList.toggle("showSlide");
+        }
+    },3000);
+
+    console.log(slideShow)
+
+});
+
+
